c7bd675b58c5ad204587cff34e4e15aca6ae2025
Lutz Eichler <frickler24@frickler24.de>
2019-11-12 09:12:01 +0100

    add burnin.sh

--- burnin.sh
+++ burnin.sh
@@ -3,6 +3,7 @@
 for p in $(nmap -sL 192.168.1.0/24 |grep Nmap | grep pi | cut -d" " -f 5 | cut -d"." -f 1 | sort -u)
 do
     echo $p
-    curl http://$p:30666/brot.php > /dev/null
+    curl http://pi1:30666/brot.php > /dev/null
 done
+
 
