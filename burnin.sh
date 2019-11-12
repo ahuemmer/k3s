d550af94c8cf0fa25c5f52f19ca86abd61711568
Lutz Eichler <frickler24@frickler24.de>
2019-11-12 16:28:34 +0100

    add health checks

--- burnin.sh
+++ burnin.sh
@@ -1,9 +1,31 @@
-#!/bin/sh
+#!/bin/bash
 
-for p in $(nmap -sL 192.168.1.0/24 |grep Nmap | grep pi | cut -d" " -f 5 | cut -d"." -f 1 | sort -u)
+MAXRUNS=1000000
+
+# PAGE=test.php
+PAGE=mandel.php
+
+singleBurn() {
+	echo $NODES
+	for p in $NODES
+	do
+			echo $p
+			(curl -s http://$p:30666/$PAGE | wc -c ; echo $p) &
+	done
+
+	echo "Wait for completion"
+	wait
+	echo "-----------------------"
+}
+
+# export NODES="$(nmap -sL 192.168.1.0/24 |grep Nmap | grep pi | cut -d" " -f 5 | cut -d"." -f 1 | sort -u | tr '\n' ' ')"
+export NODES="$(kubectl get pods -Ao wide | grep phpngx | sed "s/  */ /g" | cut -d" " -f 8 | sort -u | tr '\n' ' ')"
+
+for i in $(eval echo "{1..$MAXRUNS}");
 do
-    echo $p
-    curl http://pi1:30666/brot.php > /dev/null
+	echo starte Durchlauf $i
+	singleBurn
 done
+
 
 
