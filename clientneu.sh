a345dfcc7f6ecc4fcf7d485cca5216a8181eea32
Lutz Eichler <frickler24@frickler24.de>
2019-11-14 08:55:43 +0100

    new af-ui-yaml

--- clientneu.sh
+++ clientneu.sh
@@ -2,6 +2,8 @@
 set +x
 set +e
 
+MASTER_IP=192.168.1.12
+
 clients() {
 	echo "Starten der Worker auf den Clients"
 	set +x
@@ -17,10 +19,10 @@ clients() {
 	[[ -x /usr/local/bin/k3s-uninstall.sh ]] && k3s-uninstall.sh
 	docker rm -f $(docker ps -aq) 2>/dev/null
 
-	export K3S_URL=https://192.168.1.12:6443
-	export K3S_TOKEN=$(ssh bigengine sudo cat /var/lib/rancher/k3s/server/node-token)
-	set | grep K3S
-	curl -sfL http://bigengine:30001 | sh -s - --docker
+	export K3S_URL=https://${MASTER_IP}:6443
+	export K3S_TOKEN=$(ssh ${MASTER_IP} sudo cat /var/lib/rancher/k3s/server/node-token)
+  ssh ${MASTER_IP} cat /etc/rancher/k3s/k3s.yaml | sed "s/127\.0\.0\.1/${MASTER_IP}/g" > ~/.kube/config
+  curl -sfL http://${MASTER_IP}:30001 | sh -s - --docker
 }
 
 clients | tee ~/client.log 2>&1
--- verteilen.sh
+++ verteilen.sh
@@ -2,14 +2,17 @@
 set +x
 set +e
 
-echo "Starten der Worker auf den Clients"
+echo "Verteile Wichtiges auf den Clients"
 
-# for i in $(nmap -sL 192.168.1.0/24 |grep Nmap | grep pi | cut -d" " -f 5 | cut -d"." -f 1 | sort -u)
-for i in pi2 pi3 pi4
+for i in $(nmap -sL 192.168.1.0/24 |grep Nmap | grep pi | cut -d" " -f 5 | cut -d"." -f 1 | sort -u)
+# for i in pi2 pi3 pi4
 do
 	echo Bearbeite Pi $i
-	ssh $i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no rm -rf /home/pi/k3s
-	scp $i -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no git clone https://github.com/frickler24/k3s
+	# ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $i rm -rf /home/pi/{k3s,client.log,k8s}
+	ssh $i rm -rf /home/pi/{k3s,client.log}
+	ssh $i git clone https://github.com/frickler24/k3s
+	scp -r ~/ziai12/blink $i:./.
+	scp -r ~/.bashrc ~/.bash_aliases ~/.toprc ~/.zshrc .ssh $i:./.
 done
 
 exit 0
